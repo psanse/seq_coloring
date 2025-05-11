97ff58d0d6f534189f89f0a8852a4b69ae066fb3
pablo <pablo.sansegundo@gmail.com>
2025-05-11 13:47:06 +0200

    implementing a skeleton class for SEQ coloring

--- src/main_seq.cpp
+++ src/main_seq.cpp
@@ -9,48 +9,66 @@ using namespace std;
 
 #include <iostream>
 #include "graph/graph.h"
-//#include "graph_isomorphism.h"
+#include "bitscan/bitscan.h"
+
+
+template<class Bitset_t>
+class SEQ {
+
+	std::vector<Bitset_t> bbc_;						//list of color (bit)sets.
+	std::vector<int> lc_;							//[vertex_index] -> color number --- standard color encoding 
+
+	std::size_t nV_;								//number of vertices	
+	std::size_t maxCol_;							//maximum number of colors
+	std::size_t nCol_;								//number of colors of the current coloring
+public:
+	//construction / destruction
+	SEQ(std::size_t size, std::size_t MAX_COL = size) :
+		nV_{ size },
+		maxCol_(MAX_COL),
+		lc_{ size, 0 },
+		bbc_{ maxCol_ },							//internally, color numbers range from [0 , maxCol_)
+	{		
+		//allocate empty bitsets
+		for (const bb& : bbc_) {
+			bbc.reset(nV_);
+		}	
+	}
+
+	//copy and move constructors forbidden
+	SEQ(const SEQ&) = delete;
+	SEQ(SEQ&&) = delete;
+	~SEQ() = default;
+
+	//setters and getters
+	std::size_t number_of_colors() const { return nCol_; }
+	std::size_t number_of_vertices() const { return nV_; }
+	std::size_t maximum_number_of_colors() const { return maxCol_; }
+
+	unsigned int color(int v) { return lc_[v]; }
+
+
+	//TODO...
+
+	///////////////
+	//drivers
+
+	
+
+};
 
 int main() {
 	
-	
-	//two input simple undirected graphs
-	ugraph g1;
-	ugraph g2;
-
-	//example A
-	/*g1.reset(10);
-	g2.reset(10);	
-	g1.add_edge(0, 1);
-	g1.add_edge(0, 2);
-	g2.add_edge(3, 4);
-	g2.add_edge(3, 5);*/
-
-	//example B
-	/*g1 = ugraph::make_clique(2000);
-	g2 = ugraph::make_clique(2000);
-	g1.remove_edge(0, 1);
-	g1.remove_edge(0, 2);
-	g2.remove_edge(100, 1099);
-	g2.remove_edge(101, 1000);*/
-		
-
-	///////////////////////////////////////////////////
-	////TODO - determine if g1 and g2 are isomorphic
-	//////////////////////////////////////////////////
-	//GraphIsomorphism<> gi(g1, g2);
-
-	////computes distance invariant features for all vertices
-	//gi.compute_orderings();
-
-	////check if the two graphs are isomorphic according to the orderings
-	//if (gi.is_isomorphic(gi.get_orderings()))  {
-	//	LOG_INFO("Graphs are isomorphic");
-	//}
-	//else {
-	//	LOG_INFO("Graphs are not isomorphic - not 100%% sure but almost :-");
-	//}
-
-
 	std::cout << " hello world" << std::endl;	
+
+	//test vector of bitsets
+	//bitarray bbc{ 10, {1, 5, 7} };
+
+	std::vector<bitarray> bbc{ 2 };
+	bbc[0].reset(2);
+	bbc[1].reset(2);
+	
+	
+
+
 }
