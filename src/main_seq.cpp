218e0e003dc1cb76deecbc84a43180432b77dcb5
pablo <pablo.sansegundo@gmail.com>
2025-05-12 17:39:30 +0200

    first implementation of seq_coloring in O(n*n) using bitsets

--- src/main_seq.cpp
+++ src/main_seq.cpp
@@ -18,28 +18,23 @@ using namespace std;
 #include <cassert>
 
 
-template<class Bitset_t>
+template<class BitSet_t>
 class SEQ {
 
-	std::vector<Bitset_t> bbc_;						//list of color (bit)sets.
+	std::vector<BitSet_t> bbc_;						//list of color (bit)sets.
 	std::vector<int> lc_;							//[vertex_index] -> color number --- standard color encoding 
 
-	std::size_t nV_;								//number of vertices	
-	std::size_t maxCol_;							//maximum number of colors
-	std::size_t nCol_;								//number of colors of the current coloring
+	int nV_;								//number of vertices	
+	int maxCol_;							//maximum number of colors
+	int nCol_ = 0;							//number of colors of the current coloring
 public:
 	//construction / destruction
-	SEQ(std::size_t size, std::size_t MAX_COL = size) :
+	SEQ(int size, int MAX_COL = size) :
 		nV_{ size },
 		maxCol_{ MAX_COL },
-		lc_{ (int)size, 0 },
-		bbc_{ maxCol_ }							//internally, color numbers range from [0 , maxCol_)
-	{		
-		//allocate empty bitsets
-		for (auto& bb : bbc_) {
-			bb.reset(nV_);
-		}	
-	}
+		lc_((int)size, 0),
+		bbc_(MAX_COL, BitSet_t{ size })		//internally, color numbers range from [0 , maxCol_)
+	{}
 
 	//copy and move constructors forbidden
 	SEQ(const SEQ&) = delete;
@@ -53,7 +48,6 @@ public:
 
 	unsigned int color(int v) { return lc_[v]; }
 
-
 	//TODO...
 
 	///////////////
@@ -63,64 +57,57 @@ public:
 	* @param ug: input graph
 	* @returns : number of colors required
 	**/
-	template<class BitSet_t>
 	int seq_coloring(const ugraph& ug) {
 
-		int nC = 1;
-
-
-		//TODO...
 		int pc = ug.size();
-		std::vector<int> uncol{pc};
-		std::iota(uncol.begin(), uncol.end(), 1);			//set of vertices
-		com::stl::print_collection(uncol);
-		
+
 		///////////////////
 		assert(pc >= 1);
 		///////////////////
-
-		//int col = 1, v = bbo::noBit, nBB = bbo::noBit;
-
-		////main loop - greedy coloring	
-		//typename Graph_t::_bbt bb_unsel(bbsg);
-		//typename Graph_t::_bbt bb_sel(g.size());
-		//while (true) {
-
-		//	//load bb_sel with remaining vertices to be colored
-		//	bb_sel = bb_unsel;
-
-		//	//build a new iset with vertices from bb_sel
-		//	bb_sel.init_scan(bbo::DESTRUCTIVE);
-		//	while ((v = bb_sel.next_bit_del()) != bbo::noBit) {
-
-		//		//update coloring
-		//		if (ub) { ub[v] = col; }
-
-		//		//remove colored vertex from bbsg
-		//		bb_unsel.erase_bit(v);
-
-		//		/////////////////////////////////
-		//		if ((--pc) == 0) { return col; }				//early exit - all vertices colored
-		//		/////////////////////////////////
-
-		//		//removes neighbors of v
-		//		bb_sel.erase_block(WDIV(v), -1, g.neighbors(v));
-		//	}
-
-		//	//open a new color
-		//	++col;
-		//}
-
-		//return col;		//should not reach here
-
-
 		
+		//set list of all vertices
+		std::vector<int> uncol;
+		uncol.resize(pc);									//set of uncolored vertices
+		std::iota(uncol.begin(), uncol.end(), 0);			//set of vertices
+		
+		//I/O
+		//com::stl::print_collection(uncol);
+			
 
+		//TODO...
+		int nC = 1;
+		bool color_found;
+		for (const auto& v : uncol) {
+
+			//find first open bit color set with no vertices adjacent to v
+			color_found = false;
+			for (auto c = 1; c <= nC; ++c) {
+				int w = find_first_common_block(0, WDIV(v), ug.neighbors(v), bbc_[c]);
+				if (w == bbo::noBit) {
+
+					//enlarge color set, set color number
+					bbc_[c].set_bit(v);						
+					lc_[v] = c;	
+
+					bbc_[c].print();
+					
+					color_found = true;
+					break;
+				}
+			}
+
+			//open a new color
+			if (!color_found) {
+				++nC;
+
+				//add vertex to color set
+				bbc_[nC].set_bit(v);
+			}
+		}
 
 		return nC;
 	}
 	
-
 };
 
 int main() {
@@ -139,8 +126,14 @@ int main() {
 	ug.add_edge(1, 2);
 	ug.add_edge(2, 3);
 
-	SEQ<bitarray> sq(ug.size(), 10);
-	sq.seq_coloring<bitarray>(ug);
+	SEQ<bitarray> sq{ 5, 3 };
+	int nCol = sq.seq_coloring(ug);
+
+	cout << "number of colors: " << nCol << endl;	
+
+
+	/*std::vector<bitarray> kk{ 2 };
+	kk[0].reset(10);*/
 	
 
 
