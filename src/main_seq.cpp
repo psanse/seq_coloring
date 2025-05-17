89abe5fbe2592783ec1fc34aac39225c7fe5b0d7
pablo <pablo.sansegundo@gmail.com>
2025-05-17 13:51:07 +0200

    built a first decent base class and isetSEQ class

--- src/main_seq.cpp
+++ src/main_seq.cpp
@@ -61,6 +61,28 @@ struct BaseSeq{
 
 };
 
+template<class Grapht_t = ugraph>
+class baseSeq {
+protected:
+	const Grapht_t& ug_;				//input graph
+	std::vector<int> lc_;				//[vertex_index] -> color number --- standard color encoding 
+	int nCol_ = 0;						//number of colors of the current coloring
+
+public:
+	
+	//construction / destruction
+	baseSeq(const ugraph& ug):
+		ug_{ ug },
+		lc_(ug_.size(), 0)
+	{
+		assert(ug.size() > 0);			//assert graph is not empty
+	}
+	
+	//setters and getters
+	int number_of_colors()	const { return nCol_; }
+	int color(int v)		const { return lc_[v]; }	
+	const ugraph& graph()	const { return ug_; }
+};
 
 ///////////////////////
 //
@@ -69,54 +91,72 @@ struct BaseSeq{
 //
 ///////////////////////
 
-template<class BitSet_t>
-struct BaseIsetSeq {
-	BitSet_t bbSel_;					//color iset under construction
-	BitSet_t bbUnsel_;					//bitset of uncolored vertices.
-	std::vector<int> lc_;				//[vertex_index] -> color number --- standard color encoding 
+class seqIset : public  baseSeq<ugraph> {
 
-	int nV_;							//number of vertices
-	int nCol_ = 0;						//number of colors of the current coloring
+	using bitset = typename ugraph::_bbt;
 
+public:
 	//construction / destruction
-	BaseIsetSeq(int size) :
-		bbSel_{ size },
-		bbUnsel_{ size },
-		lc_(size, 0)
+	seqIset(const ugraph& ug) : baseSeq(ug),
+		bbSel_{ (int)ug_.size() },
+		bbUnsel_{ (int)ug_.size() , true }
 	{}
-			
+	
+	/**
+	* @brief computes greedy sequential coloring for the induced subgraph by the (bit) set @pBBsg
+	*		 in the graph @ug_. if @pBBsg is nullptr, computes a coloring for @ug_	* 
+	* @returns: number of colors used in the coloring
+	**/
+	int seq_coloring(const bitset* pBBsg = nullptr) {
 
-	//setters and getters	
-	int number_of_vertices_colored() const { return nV_; }
-	int number_of_colors() const { return nCol_; }
+		//sets the subgraph to be colored in bbUnsel_
+		if (pBBsg) { bbUnsel_ = *pBBsg; }
+		else { bbUnsel_.set_bit(0, ug_.size()-1); }	//all vertices are colored
 
-	//interface
-	virtual int seq_coloring(const ugraph& ug, BitSet_t bbsg) = 0;
-	virtual int seq_coloring(const ugraph& ug, std::vector<int> lvsg) = 0;
-	virtual int seq_coloring(const ugraph& ug) = 0;
+		this->nCol_ = 1;
+		int pc = bbUnsel_.size();
+		int v = bbo::noBit;
+		while (true) {
 
+			//set bb_sel to the remaining vertices to be colored
+			this->bbSel_ = this->bbUnsel_;
+
+			//build a new iset with vertices from bb_sel
+			this->bbSel_.init_scan(bbo::NON_DESTRUCTIVE);
+			while ((v = this->bbSel_.next_bit()) != bbo::noBit) {
+
+				//update coloring
+				this->lc_[v] = this->nCol_;
+
+				//early exit - all vertices colored
+				if ((--pc) == 0) { return this->nCol_; }
+
+
+				//removes neighbors of v from the color set under construction
+				this->bbSel_.erase_block(WDIV(v), -1, ug_.neighbors(v));
+
+				//remove colored vertex from uncolored set
+				this->bbUnsel_.erase_bit(v);
+			}
+
+			//open a new color
+			++this->nCol_;
+		}
+
+		///////////////////	
+		assert(false);				//should not reach here
+		//////////////////
+
+		return this->nCol_;
+	}
+
+	/////////
+	//data members
+private:
+	bitset bbSel_;					//color iset under construction
+	bitset bbUnsel_;					//set of uncolored vertices
 };
 
-
-template<class BitSet_t>
-class seq : public BaseSeq<BitSet_t> {
-
-	//inherit constructor
-	using BaseSeq<BitSet_t>::BaseSeq;
-
-	//TODO...
-
-};
-
-template<class BitSet_t>
-class seqIset : public BaseIsetSeq<BitSet_t> {
-
-	//inherit constructor
-	using BaseIsetSeq<BitSet_t>::BaseIsetSeq;
-
-	//TODO...
-
-};
 							
 template<class BitSet_t>
 class SEQ {
@@ -211,25 +251,42 @@ public:
 };
 
 int main() {
-	
-	std::cout << " hello world" << std::endl;	
+	const int SIZE = 5;
 
-	//test vector of bitsets
-	//bitarray bbc{ 10, {1, 5, 7} };
-
-	/*std::vector<bitarray> bbc{ 2 };
-	bbc[0].reset(2);
-	bbc[1].reset(2);*/
-	
-	ugraph ug{ 5 };
+	ugraph ug{ SIZE };
 	ug.add_edge(0, 1);
 	ug.add_edge(1, 2);
 	ug.add_edge(2, 3);
+	ug.add_edge(1, 3);
 
-	SEQ<bitarray> sq{ 5, 3 };
-	int nCol = sq.seq_coloring(ug);
+	//A)
+	//SEQ<bitarray> sq{ 5, 3 };
+	//int nCol = sq.seq_coloring(ug);
 
-	cout << "number of colors: " << nCol << endl;	
+
+	//B)
+	//seqIset<bitarray> sqI(SIZE);
+	//int nCol = sqI.seq_coloring(ug);
+
+	//cout << "number of colors: " << nCol << endl;	
+	//assert(sqI.color(0) == 1);
+	//assert(sqI.color(1) == 2);
+	//assert(sqI.color(2) == 1);
+	//assert(sqI.color(3) == 3);
+
+	//C)
+	typename ugraph::_bbt bbsg{ SIZE, {1, 2, 4} };
+
+	bbsg.print();
+
+	seqIset sqI(ug);
+	int nCol = sqI.seq_coloring(&bbsg);
+
+	cout << "number of colors: " << nCol << endl;
+	assert(sqI.color(1) == 1);
+	assert(sqI.color(2) == 2);
+	assert(sqI.color(4) == 1);
+
 
 
 	/*std::vector<bitarray> kk{ 2 };
--- src/tests/test_seq_coloring.cpp
+++ src/tests/test_seq_coloring.cpp
@@ -8,12 +8,43 @@
 #include "gtest/gtest.h"
 #include "graph/graph.h"
 
+
 #include <iostream>
 
 using namespace std;
 
-TEST(Coloring, SEQ){
-
-   //TODO....
-
-}
+//
+//
+//TEST(Coloring, SEQ_Iset) {
+//
+//
+//	const int NV = 6;
+//
+//	ugraph ug(NV);
+//	ug.add_edge(0, 1);
+//	ug.add_edge(0, 3);
+//	ug.add_edge(0, 4);
+//	ug.add_edge(2, 5);
+//	ug.add_edge(3, 4);
+//
+//
+//	SEQ
+//
+//	std::vector<int> clq;
+//	//typename ugraph::_bbt bbsg{ static_cast<int>(ug.size()), { 0, 1, 2, 3, 4, 5} };
+//
+//
+//	////////////////////////////////////////
+//	int ub[NV];
+//	int nCol = gfunc::clq::SEQ(ug, bbsg, ub);
+//	///////////////////////////////////////
+//
+//	EXPECT_EQ(3, nCol);		//{0, 2} {1, 3, 5} {4} - 3 colors
+//	EXPECT_EQ(1, ub[0]);
+//	EXPECT_EQ(1, ub[2]);
+//	EXPECT_EQ(2, ub[1]);
+//	EXPECT_EQ(2, ub[3]);
+//	EXPECT_EQ(2, ub[5]);
+//	EXPECT_EQ(3, ub[4]);
+//
+//}
